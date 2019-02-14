6324e5ce26bec2580f7679bba8848fd6adaa5dbe
Stefan Armbruster <stefan@armbruster-it.de>
2019-02-14 15:58:00 +0100

    change tests to use TestGraphDatabaseFactory instead of neo4jrule

--- src/test/java/org/neo4j/asyncaction/ProceduresTests.java
+++ src/test/java/org/neo4j/asyncaction/ProceduresTests.java
@@ -1,19 +1,23 @@
 package org.neo4j.asyncaction;
 
-import org.junit.Assert;
-import org.junit.Rule;
-import org.junit.Test;
+import org.junit.*;
 import org.neo4j.driver.internal.util.Iterables;
 import org.neo4j.driver.v1.AccessMode;
 import org.neo4j.driver.v1.Driver;
 import org.neo4j.driver.v1.GraphDatabase;
 import org.neo4j.driver.v1.Session;
+import org.neo4j.graphdb.GraphDatabaseService;
 import org.neo4j.graphdb.Relationship;
 import org.neo4j.graphdb.Transaction;
-import org.neo4j.harness.junit.Neo4jRule;
+import org.neo4j.helpers.ListenSocketAddress;
 import org.neo4j.helpers.collection.Iterators;
 import org.neo4j.helpers.collection.MapUtil;
+import org.neo4j.internal.kernel.api.exceptions.KernelException;
+import org.neo4j.kernel.configuration.BoltConnector;
 import org.neo4j.kernel.internal.GraphDatabaseAPI;
+import org.neo4j.logging.AssertableLogProvider;
+import org.neo4j.ports.allocation.PortAuthority;
+import org.neo4j.test.TestGraphDatabaseFactory;
 
 import java.util.ArrayList;
 import java.util.Collections;
@@ -29,25 +33,54 @@ import static org.junit.Assert.assertTrue;
 
 public class ProceduresTests {
 
-    @Rule
-    public Neo4jRule neo4j = new Neo4jRule().withProcedure(Procedures.class);
+    private GraphDatabaseAPI db;
+    private String boltUrl;
+
+    @Before
+    public void startDatabase() throws KernelException {
+        BoltConnector connector = new BoltConnector("bolt");
+        int port = PortAuthority.allocatePort();
+        boltUrl = "bolt://localhost:" + port;
+        db = (GraphDatabaseAPI) new TestGraphDatabaseFactory()
+                .setUserLogProvider(new AssertableLogProvider(true))
+                .newImpermanentDatabaseBuilder()
+                .setConfig(connector.listen_address, ListenSocketAddress.listenAddress("0.0.0.0", port))
+                .setConfig(connector.enabled, "true")
+                .newGraphDatabase();
+        registerProcedure(db, Procedures.class);
+    }
+    
+    @After
+    public void stopDatabase() {
+        finishQueueAndWait();        
+        db.shutdown();
+    }
+
+    public static void registerProcedure(GraphDatabaseService db, Class<?>...procedures) throws KernelException {
+        org.neo4j.kernel.impl.proc.Procedures proceduresService = ((GraphDatabaseAPI) db).getDependencyResolver().resolveDependency(org.neo4j.kernel.impl.proc.Procedures.class);
+        for (Class<?> procedure : procedures) {
+            proceduresService.registerProcedure(procedure,true);
+            proceduresService.registerFunction(procedure, true);
+            proceduresService.registerAggregationFunction(procedure, true);
+        }
+    }
 
     @Test
     public void verifyNoDeadlock() {
 
-        final long languageNodeId = Iterators.single(neo4j.getGraphDatabaseService().execute("CREATE (a:LANGUAGE) RETURN id(a) as id").columnAs("id"));
+        final long languageNodeId = Iterators.single(db.execute("CREATE (a:LANGUAGE) RETURN id(a) as id").columnAs("id"));
 
         List<Long> patentIds = new ArrayList<>();
 
-        try (Transaction tx = neo4j.getGraphDatabaseService().beginTx()) {
+        try (Transaction tx = db.beginTx()) {
             for (int i = 0; i < 100; i++) {
-                final Long nodeId = neo4j.getGraphDatabaseService().createNode().getId();
+                final Long nodeId = db.createNode().getId();
                 patentIds.add(nodeId);
             }
             tx.success();
         }
 
-        try (final Driver driver = GraphDatabase.driver(neo4j.boltURI())) {
+        try (final Driver driver = GraphDatabase.driver(boltUrl)) {
             patentIds.parallelStream().forEach((patentId) -> {
 
                 try (Session session = driver.session(AccessMode.WRITE)) {
@@ -66,19 +99,19 @@ public class ProceduresTests {
 
         finishQueueAndWait();
 
-        long count = Iterators.single(neo4j.getGraphDatabaseService().execute("MATCH ()-[r:HAS]->() RETURN count(r) AS count").columnAs("count"));
+        long count = Iterators.single(db.execute("MATCH ()-[r:HAS]->() RETURN count(r) AS count").columnAs("count"));
         Assert.assertEquals(100, count);
     }
 
     @Test
     public void verifyPropertiesAreAddedTest() {
 
-        neo4j.getGraphDatabaseService().execute("CREATE (a), (b) WITH a,b UNWIND range(1,20) AS x CALL async.createRelationship(a, b, 'KNOWS', {test : 2}) RETURN a,b");
+        db.execute("CREATE (a), (b) WITH a,b UNWIND range(1,20) AS x CALL async.createRelationship(a, b, 'KNOWS', {test : 2}) RETURN a,b");
         finishQueueAndWait();
 
-        for (Map<String, Object> result : Iterators.asIterable(neo4j.getGraphDatabaseService().execute("MATCH ()-[r:KNOWS]->() RETURN r"))) {
+        for (Map<String, Object> result : Iterators.asIterable(db.execute("MATCH ()-[r:KNOWS]->() RETURN r"))) {
 
-            final Transaction transaction = neo4j.getGraphDatabaseService().beginTx();
+            final Transaction transaction = db.beginTx();
 
             Assert.assertEquals(1, result.keySet().size());
             Assert.assertEquals(1, ((Relationship) result.get("r")).getAllProperties().keySet().size());
@@ -92,10 +125,10 @@ public class ProceduresTests {
     @Test
     public void shouldAsyncCreationOfRelationshipsWork() {
 
-        neo4j.getGraphDatabaseService().execute("CREATE (a), (b) WITH a,b UNWIND range(1,20) AS x CALL async.createRelationship(a, b, 'KNOWS', {}) RETURN a,b");
+        db.execute("CREATE (a), (b) WITH a,b UNWIND range(1,20) AS x CALL async.createRelationship(a, b, 'KNOWS', {}) RETURN a,b");
         finishQueueAndWait();
 
-        long count = Iterators.single(neo4j.getGraphDatabaseService().execute("MATCH ()-[r:KNOWS]->() RETURN count(r) AS count").columnAs("count"));
+        long count = Iterators.single(db.execute("MATCH ()-[r:KNOWS]->() RETURN count(r) AS count").columnAs("count"));
         assertEquals(20, count);
 
     }
@@ -103,62 +136,62 @@ public class ProceduresTests {
     @Test
     public void shouldAsyncCreationOfRelationshipViaCypherWork() {
 
-        neo4j.getGraphDatabaseService().execute("CREATE (a), (b) \n" +
+        db.execute("CREATE (a), (b) \n" +
                 "WITH a,b\n" +
                 "UNWIND range(1,20) as x\n" +
                 "CALL async.cypher(\"with $start as start, $end as end create (start)-[:KNOWS]->(end)\", {start:a, end:b})\n" +
                 "RETURN a,b");
         finishQueueAndWait();
 
-        long count = Iterators.single(neo4j.getGraphDatabaseService().execute("MATCH ()-[r:KNOWS]->() RETURN count(r) AS count").columnAs("count"));
+        long count = Iterators.single(db.execute("MATCH ()-[r:KNOWS]->() RETURN count(r) AS count").columnAs("count"));
         assertEquals(20, count);
     }
 
     @Test
     public void shouldConnectingToDenseNodeWorkWithCypher() {
 
-        neo4j.getGraphDatabaseService().execute("CREATE INDEX ON :Person(id)");
-        neo4j.getGraphDatabaseService().execute("MERGE (dense:Person{id:'dense'})");
+        db.execute("CREATE INDEX ON :Person(id)");
+        db.execute("MERGE (dense:Person{id:'dense'})");
 
         boolean regularTermination = runWithExecutorService(1000, o ->
-                neo4j.getGraphDatabaseService().execute("MERGE (dense:Person{id:'dense'}) \n" +
+                db.execute("MERGE (dense:Person{id:'dense'}) \n" +
                 "MERGE (rnd:Person{id:'person_' +toInt(rand()*1000)})\n" +
                 "MERGE (rnd)-[:KNOWS]->(dense)"));
 
         assertTrue(regularTermination);
 
-        long count = Iterators.single(neo4j.getGraphDatabaseService().execute("MATCH (p:Person) RETURN count(p) AS c").columnAs("c"));
+        long count = Iterators.single(db.execute("MATCH (p:Person) RETURN count(p) AS c").columnAs("c"));
         assertTrue(count < 1000);
 
-        count = Iterators.single(neo4j.getGraphDatabaseService().execute("MATCH (p:Person{id:'dense'}) RETURN size((p)<-[:KNOWS]-()) AS c").columnAs("c"));
+        count = Iterators.single(db.execute("MATCH (p:Person{id:'dense'}) RETURN size((p)<-[:KNOWS]-()) AS c").columnAs("c"));
         assertTrue(count < 1000);
     }
 
     @Test
     public void testConnectingToDenseNodeUsingAsync() {
 
-        neo4j.getGraphDatabaseService().execute("CREATE INDEX ON :Person(id)");
-        neo4j.getGraphDatabaseService().execute("MERGE (dense:Person{id:'dense'})");
-        boolean regularTermination = runWithExecutorService(1000, index -> neo4j.getGraphDatabaseService().execute("MERGE (dense:Person{id:'dense'}) \n" +
+        db.execute("CREATE INDEX ON :Person(id)");
+        db.execute("MERGE (dense:Person{id:'dense'})");
+        boolean regularTermination = runWithExecutorService(1000, index -> db.execute("MERGE (dense:Person{id:'dense'}) \n" +
                 "    CREATE (rnd:Person{id:$id})\n" +
                 "    WITH dense, rnd \n" +
                 "    CALL async.createRelationship(rnd, dense, 'KNOWS', {}) \n" +
                 "    RETURN dense, rnd", Collections.singletonMap("id", "person_" + index)));
         assertTrue(regularTermination);
 
-        long count = Iterators.single(neo4j.getGraphDatabaseService().execute("MATCH (p:Person) RETURN count(p) AS c").columnAs("c"));
+        long count = Iterators.single(db.execute("MATCH (p:Person) RETURN count(p) AS c").columnAs("c"));
         assertEquals(1001, count);
 
-        count = Iterators.single(neo4j.getGraphDatabaseService().execute("MATCH (p:Person{id:'dense'}) RETURN size((p)<-[:KNOWS]-()) AS c").columnAs("c"));
+        count = Iterators.single(db.execute("MATCH (p:Person{id:'dense'}) RETURN size((p)<-[:KNOWS]-()) AS c").columnAs("c"));
         assertEquals(1000, count);
     }
 
     @Test
     public void testConnectingToDenseNodeUsingAsyncMerge() {
 
-        neo4j.getGraphDatabaseService().execute("CREATE INDEX ON :Person(id)");
-        neo4j.getGraphDatabaseService().execute("MERGE (dense:Person{id:'dense'})");
-        boolean regularTermination = runWithExecutorService(1000, index -> neo4j.getGraphDatabaseService().execute("MERGE (dense:Person{id:'dense'}) \n" +
+        db.execute("CREATE INDEX ON :Person(id)");
+        db.execute("MERGE (dense:Person{id:'dense'})");
+        boolean regularTermination = runWithExecutorService(1000, index -> db.execute("MERGE (dense:Person{id:'dense'}) \n" +
                 "    MERGE (rnd:Person{id:'person_' +toInt(rand()*1000)})\n" +
                 "    WITH dense, rnd \n" +
                 "    CALL async.mergeRelationship(rnd, dense, 'KNOWS') \n" +
@@ -166,17 +199,16 @@ public class ProceduresTests {
 
         assertTrue(regularTermination);
 
-        long count = Iterators.single(neo4j.getGraphDatabaseService().execute("MATCH (p:Person) RETURN count(p) AS c").columnAs("c"));
+        long count = Iterators.single(db.execute("MATCH (p:Person) RETURN count(p) AS c").columnAs("c"));
         assertTrue(count < 800);
 
-        count = Iterators.single(neo4j.getGraphDatabaseService().execute("MATCH (p:Person{id:'dense'}) RETURN size((p)<-[:KNOWS]-()) AS c").columnAs("c"));
+        count = Iterators.single(db.execute("MATCH (p:Person{id:'dense'}) RETURN size((p)<-[:KNOWS]-()) AS c").columnAs("c"));
         assertTrue(count < 800);
 
     }
 
     @Test
     public void ensureQueueGettingFlushedAndNotAppliedUponRollback() {
-        GraphDatabaseAPI db = (GraphDatabaseAPI) neo4j.getGraphDatabaseService();
         try (Transaction tx = db.beginTx()) {
             AsyncQueueHolder asyncQueueHolder = db.getDependencyResolver().resolveDependency(AsyncQueueHolder.class);
             asyncQueueHolder.add((graphDatabaseService, log) -> graphDatabaseService.createNode());
@@ -192,7 +224,6 @@ public class ProceduresTests {
 
     @Test
     public void ensureTxWithoutAsyncActionWorks() {
-        GraphDatabaseAPI db = (GraphDatabaseAPI) neo4j.getGraphDatabaseService();
         try (Transaction tx = db.beginTx()) {
             db.createNode();
             tx.success();
@@ -229,7 +260,7 @@ public class ProceduresTests {
     }
 
     private void finishQueueAndWait() {
-        AsyncQueueHolder asyncQueueHolder = ((GraphDatabaseAPI) neo4j.getGraphDatabaseService()).getDependencyResolver().resolveDependency(AsyncQueueHolder.class);
+        AsyncQueueHolder asyncQueueHolder = db.getDependencyResolver().resolveDependency(AsyncQueueHolder.class);
         asyncQueueHolder.stop();
         assertTrue(asyncQueueHolder.isQueueEmpty());
         assertTrue(asyncQueueHolder.isInBoundEmpty());
